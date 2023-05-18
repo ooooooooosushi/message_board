049ae77cce3497a8ee8717be8baadea9d23a9679
ryosuke.harada <ryosuke.nnlife@outlook.jp>
2023-05-19 08:59:14 +0900

    Index/NewServletの修正

--- src/controllers/NewServlet_sub.java
+++ src/controllers/NewServlet_sub.java
@@ -1,8 +1,9 @@
 package controllers;
 
 import java.io.IOException;
+import java.sql.Timestamp;
 
-import javax.servlet.RequestDispatcher;
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,18 +11,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Message;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class NewServlet
  */
-@WebServlet("/new")
-public class NewServlet extends HttpServlet {
+@WebServlet("/newaaaaaa")
+public class NewServlet_sub extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public NewServlet() {
+    public NewServlet_sub() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -58,15 +60,32 @@ public class NewServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doget");
+		EntityManager em = DBUtil.createEntityManager();
+		em.getTransaction().begin();
 
-		// CSRF対策
-		request.setAttribute("_token", request.getSession().getId());
+		// Messageのインスタンスを作成
+		Message m = new Message();
 
-		// おまじないとしてのインスタンスを生成
-		request.setAttribute("message", new Message());
+		// mの各フィールドにデータを代入
+		String title = "taro";
+		m.setTitle(title);
 
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
-		rd.forward(request, response);
+		String content = "hello";
+		m.setContent(content);
+
+		Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+		m.setCreated_at(currentTime);
+		m.setUpdated_at(currentTime);
+
+		// データベースに保存
+		em.persist(m);
+		em.getTransaction().commit();
+
+		// 自動採番されたIDの値を表示
+		response.getWriter().append(Integer.valueOf(m.getId()).toString());
+
 	}
 
 }
